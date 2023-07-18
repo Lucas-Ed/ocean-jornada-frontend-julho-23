f2b3f2dc5ff0c13444ac67cb38a6f0966db88f22
Lucas-Ed <lucas_tranpo@yahoo.com.br>
2023-07-18 11:47:08 -0300

    tree cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+// import './components/Card/Card.css'
 import Card from './components/card/card'
 
 function App() {
@@ -6,11 +7,9 @@ function App() {
 
   return (
     <>
-    <div className='card'><h1>Título do item </h1>
-    <img src="https://static.tvtropes.org/pmwiki/pub/images/abcb6534_7913_4eb1_a7a5_62b081ebc628.png"/>
-    </div>
     <Card />
-    
+    <Card />
+    <Card />
 
     </>
   )
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,7 +1,11 @@
 //crie um componente react?
 // import React from "react";
-
+import './Card.css'
 export default function Card() {
-    return <>Teste do Card</>;
+    return (<>
+        <div className='card'><h1>Título do item </h1>
+    <img src="https://static.tvtropes.org/pmwiki/pub/images/abcb6534_7913_4eb1_a7a5_62b081ebc628.png"/>
+    </div>
+    </>);
 }
 
